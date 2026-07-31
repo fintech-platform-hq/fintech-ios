c130a209e94843f7cd4689aa266f014470cb15df
ghabrielferrari <ghcferrari@gmail.com>
2026-07-31 00:36:53 -0300

    chore: create initial iOS project

---
+++ Fintech/FintechApp.swift
@@ -0,0 +1,17 @@
+//
+//  FintechApp.swift
+//  Fintech
+//
+//  Created by Gabriel Ferrari on 31/07/26.
+//
+
+import SwiftUI
+
+@main
+struct FintechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
